a824f2b58309bb480d41c2445b6fd12c4e4e19e8
timxt <timxt@MacBook-Pro-timxt.local>
2024-06-17 22:43:04 +0300

    Add sqlal and asyncio

--- patient-app/main.py
+++ patient-app/main.py
@@ -7,6 +7,10 @@ from core.config import settings
 app = FastAPI()
 app.include_router(api_router, prefix=settings.api.prefix)
 
-
 if __name__ == '__main__':
-    uvicorn.run('main:app', reload=True)
+    uvicorn.run(
+        'main:app',
+        reload=True,
+        host=settings.run.host,
+        port=settings.run.port,
+    )
