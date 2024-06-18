ddf99b4269d6d088bb64924f27b4f448b490e083
timxt <timxt@MacBook-Pro-timxt.local>
2024-06-18 12:54:47 +0300

    Add config reed from .env and made env.template, db_helper dispose to event main

--- patient-app/main.py
+++ patient-app/main.py
@@ -1,15 +1,29 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 from api import router as api_router
 
 from core.config import settings
+from core.models.db_helper import db_helper
 
-app = FastAPI()
-app.include_router(api_router, prefix=settings.api.prefix)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await db_helper.dispose()
+
+main_app = FastAPI(
+    lifespan=lifespan
+)
+main_app.include_router(api_router, prefix=settings.api.prefix)
+
 
 if __name__ == '__main__':
     uvicorn.run(
-        'main:app',
+        'main:main_app',
         reload=True,
         host=settings.run.host,
         port=settings.run.port,
