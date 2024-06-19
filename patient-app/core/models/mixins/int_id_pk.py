706a70ac2a2c15436e430c987e62fc52196c7d1a
timxt <timxt@MacBook-Pro-timxt.local>
2024-06-19 13:01:52 +0300

    create int id pk mixin

---
+++ patient-app/core/models/mixins/int_id_pk.py
@@ -0,0 +1,7 @@
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class IdIntPKMixin:
+
+    id: Mapped[int] = mapped_column(primary_key=True)
--- patient-app/core/models/user.py
+++ patient-app/core/models/user.py
@@ -2,9 +2,10 @@ from sqlalchemy import UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
+from .mixins.int_id_pk import IdIntPKMixin
 
 
-class User(Base):
+class User(IdIntPKMixin, Base):
     username: Mapped[str] = mapped_column(unique=True, nullable=False)
     foo: Mapped[int]
     bar: Mapped[int]
